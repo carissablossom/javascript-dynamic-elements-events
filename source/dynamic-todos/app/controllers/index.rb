ac271dd694f1bae9c8de449e9f33b9092832ee81
Alex Cusack <cusackalex@gmail.com>
2015-06-16 19:51:49 -0700

    ajax walkthrough

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,9 +1,48 @@
+# Ajax is the intermediary language
+# Ajax is a method that comes along with Jquery
+# Ajax effectively targets just very specific items to refresh.
+# Ajax still runs the route
+# Ajax gets the requested reponse and updates the DOM via Jquery
+# Ajax allows live refreshing of javscript data but also a maintaining of state
+# Json is just a hash. It's the universal language that serves as a common denominator
+# remember passing around patrials for assessment
+#
+# Example of a request
+
+# $('#list').on('click', function(e){
+#   e.preventDefault();
+#   var request = @.ajax({
+#     url: "list",
+#     type: "GET"
+#     data: {key: value} #<-- this is equivilent of params
+#     });
+
+#   request.done(function(response){
+#     $('footer').append("'<div>'response'</div>'")
+#     console.lgo("success")
+#     });
+
+#   request.fail(function(response){
+#     console.log(response)
+#     console.log("you failed!")
+#     });
+#   });
+
+#####################################################
+
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/add_todo' do
-  p "Inside /add_todo route!"
+  p "in the controller!"
+  p params
+  @todo = Todo.new(todo_content: params[:content])
+  if @todo.save
+    {content: @todo.todo_content}.to_json
+  # else
+  #   rerender page with erros
+  end
 end
 
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -4,9 +4,38 @@ $(document).ready(function() {
 
 
 function bindEvents() {
-  // Bind functions which add, remove, and complete todos to the appropriate
-  // elements
-}
+  $('#todo').on('click', '#create-todo', function(e){
+    e.preventDefault();
+    var action = $('#todo-form').attr('action');
+    var method = $('#todo-form').attr('method');
+    var item = $('.todo').val();
+    console.log("wer're clicking submit and preventing default");
+    console.log(action);
+
+    var request = $.ajax({
+      url: action,
+      type: method,
+      data: {content: item}, //<--- content is the key. key needs to match key in params passed in post route.
+      dataType: 'JSON' //<--- you can send any type of string. Best habit is to just deal with it in JSON.
+    });
+
+    request.done(function(response){
+      console.log("Success")
+      console.log(response);
+      console.log(response['content']);
+      var finalContent = buildTodo(response['content']); //<-- response is just a Json hash
+      $('.todo_list').append(finalContent) //<-- adding the new todo to our HTML
+      $('.todo').val('') //<-- clearing the field after appending
+    });
+
+    request.fail(function(response){
+      console.log("FAIL")
+      console.log(response);
+    });
+  });
+};
+
+
 
 function buildTodo(todoName) {
   // gets todoTemplate stored in DOM.
