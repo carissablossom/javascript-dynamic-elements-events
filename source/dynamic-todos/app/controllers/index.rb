23709aedd73be7ee0694538f0835a48d45e6b403
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-06-21 14:12:52 -0700

    Add functionality for creation and deletion

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -5,5 +5,17 @@ end
 
 post '/add_todo' do
   p "Inside /add_todo route!"
+  @todo = Todo.new(todo_content: params[:content])
+  if @todo.save
+    return {name: @todo.todo_content, id: @todo.id}.to_json
+  else
+    p "failed"
+  end
 end
 
+delete '/' do
+  @id = params[:id]
+  @todo = Todo.find(@id)
+  @todo.destroy
+  return {id: @id}.to_json
+end
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -3,12 +3,61 @@ $(document).ready(function() {
 });
 
 
+
 function bindEvents() {
   // Bind functions which add, remove, and complete todos to the appropriate
   // elements
-}
+  $('form').on('submit', function(event){
+    event.preventDefault();
 
-function buildTodo(todoName) {
+    var path = '/add_todo';
+    var note = $('.todo').val();
+
+    var request = $.ajax({
+      url: path,
+      type: "POST",
+      dataType: 'JSON',
+      data: {content: note}
+    });
+
+    request.done(function(response){
+
+      var newTodo = buildTodo(response.name, response.id);
+      $('.todo_list').append(newTodo);
+    });
+
+    request.fail(function(response){
+      console.log("failed");
+      alert("failed");
+    });
+  });
+
+  $('.todo_list').on("click", ".delete", function(event){
+    event.preventDefault();
+
+    var path = "/"
+    var thisId = $(this).first().parent().parent().parent().attr('id');
+
+    var request = $.ajax({
+      url: path,
+      dataType: 'JSON',
+      type: "DELETE",
+      data: {id: thisId}
+    });
+
+    request.done(function(response){
+      $('#'+thisId).remove();
+    });
+
+    request.fail(function(response){
+      console.log("fail");
+      alert("fail");
+    });
+  });
+
+};
+
+function buildTodo(todoName, id) {
   // gets todoTemplate stored in DOM.
   var todoTemplate = $.trim($('#todo_template').html());
   // Creates an jQueryDOMElement from the todoTemplate.
@@ -16,7 +65,8 @@ function buildTodo(todoName) {
   // Modifies it's text to use the passed in todoName.
   $todo.find('h2').text(todoName);
   // Returns the jQueryDOMElement to be used elsewhere.
+  $todo.attr('id', ''+id);
   return $todo;
-}
+};
 
 //Create functions to add, remove and complete todos
