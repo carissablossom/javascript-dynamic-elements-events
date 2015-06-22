2711efabb03e12c40dfdc1f711902819719aca7a
James Newman <jamesmnewman1@gmail.com>
2015-06-22 09:30:27 -0700

    cant get complete to work

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,9 +1,30 @@
 get '/' do
   # Look in app/views/index.erb
+  @todo = Todo.all
   erb :index
 end
 
+#add
 post '/add_todo' do
-  p "Inside /add_todo route!"
+  todo = Todo.new(todo_content: params[:content])
+  if todo.save
+    {todo_content: todo.todo_content}.to_json
+  end
 end
 
+#delete
+delete '/add_todo/:id' do 
+ todo = Todo.find(params[:id])
+ if todo.destroy
+  content_type :json
+  {}.to_json
+ end
+end
+
+#complete
+post '/:id' do
+  todo = Todo.find(params[:id])
+  todo.completed = true
+  todo.save
+  return {completed: true}.to_json
+end
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -1,12 +1,96 @@
 $(document).ready(function() {
   bindEvents();
+  deleteTodos();
 });
 
+// add
+bindEvents = function() {
+  $('#todo').on('click', '#create-todo', function(e){
+    e.preventDefault();
+    var action = $('#todo-form').attr('action');
+    var method = $('#todo-form').attr('method');
+    var item = $('.todo').val();
+    var request = $.ajax({
+      url: action,
+      type: method,
+      data: {content: item},
+      dataType: 'json'
+    });
+    request.done(function(r){
+      console.log('success!');
+      var content = buildTodo(r['content']);
+      $('.todo_list').append(content)
+      $('.todo').val('')
+    });
 
-function bindEvents() {
-  // Bind functions which add, remove, and complete todos to the appropriate
-  // elements
+    request.fail(function(r){
+      console.log('failed!');
+    })
+  });
 }
+
+//remove/delete
+deleteTodos = function() {
+  $('.delete').on('click', function(e) {
+
+    e.preventDefault();
+    var path = $(this).attr('href');
+    var todo = $(this).closest('.todo');
+
+    var request = $.ajax({
+      method: "DELETE",
+      url: path,
+      dataType: "json"
+    });
+
+    request.done(function(r){
+      console.log(r);
+      console.log("deleted");
+      todo.remove();
+    });
+  });
+}
+
+// complete (mark button as completed)
+function completeTodos() {
+  $('.complete').on('click', function(e){
+    e.preventDefault();
+
+    var todo = $(this).closest('.todo');
+    var path = $(this).attr('href');
+
+    var request = $.ajax({
+      method: "POST",
+      url: path,
+      dataType: "json"
+    });
+
+    request.done(function(response){
+      console.log(response);
+      console.log('success??');
+      todo.find('.complete').text(response.completed);
+    })
+  })
+
+}
+
+// button_click = function() { $('.vote-button').on('click', function(e) {
+//   e.preventDefault();
+
+//   var article = $(this).closest('article');
+//   var path = $(this).attr('href');
+
+//   var request = $.ajax({
+//     method: 'POST',
+//     url: path,
+//     dataType: 'json'
+//   });
+
+//   request.done(function(response){
+//     article.find('.points').text(response.vote_count);
+//   });
+//  });
+// }
 
 function buildTodo(todoName) {
   // gets todoTemplate stored in DOM.
