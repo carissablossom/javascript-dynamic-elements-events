cab2ec8efb766409d9d03d79670581bdc3205545
Arielle Chen & Arielle Chen <pair+achen116.achen116@devbootcamp.com>
2015-06-22 19:57:26 -0700

    refactor delete listener so it deletes specific todo, not just first one

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,4 +1,5 @@
 get '/' do
+  @todos = Todo.all
   erb :index
 end
 
@@ -6,28 +7,28 @@ post '/add_todo' do
   new_todo = Todo.new(todo_content: params[:content])
 
   if new_todo.save
-  	return {content: new_todo.todo_content}.to_json
+  	return {content: new_todo.todo_content, id: new_todo.id}.to_json
   else
   	erb :index
   end
 end
 
-delete '/todo/:id' do
+delete '/delete_todo' do
   todo = Todo.where(todo_content: params[:todo]).first
-	todo.destroy()
+  todo.destroy()
 
-	return { todo_id: todo.id, todo_name: todo.todo_content }.to_json
+  return {todo_name: todo.todo_content}.to_json
 end
 
-# post '/:id' do
-# 	p "*" * 100
-# 	p 'in complete todo'
-# 	p params
-# 	p "*" * 100
+post '/:id' do
+	p "*" * 100
+	p 'in complete todo'
+	p params
+	p "*" * 100
 
-# 	todo = Todo.where(todo_content: params[:todo]).first
+	todo = Todo.where(todo_content: params[:todo]).first
 
-# 	return { todo_id: todo.id, todo_name: todo.todo_content }.to_json
+	return { todo_id: todo.id, todo_name: todo.todo_content }.to_json
 
 
-# end
+end
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -1,90 +1,99 @@
 $(document).ready(function() {
-  bindEvents();
+  // bindEvents();
+  addToDoButtonListener();
+  deleteToDoButtonListener();
+  // completeTodoButtonListener();
 });
 
 
-function bindEvents() {
+// function bindEvents() {
 
-  // var addToDoButtonListener
-  $('#todo').on('click', '#add-todo-button', function(event) {
-    event.preventDefault();
+  var addToDoButtonListener = function() {
+    $('#todo').on('click', '#add-todo-button', function(event) {
+      event.preventDefault();
 
-    var route = $('#add-todo-form').attr('action');
-    var method = $('#add-todo-form').attr('method')
-    var todo_data = $('.todo').val()
+      var route = $('#add-todo-form').attr('action');
+      var method = $('#add-todo-form').attr('method')
+      var todo_data = $('.todo').val()
 
-    var request = $.ajax({
-      url: route,
-      type: method,
-      dataType: 'json',
-      data: {content: todo_data}
-    })
+      var request = $.ajax({
+        url: route,
+        type: method,
+        dataType: 'json',
+        data: {content: todo_data}
+      })
 
-    request.done(function(response) {
-      console.log('SUCCESS', response)
+      request.done(function(response) {
+        console.log('SUCCESS', response)
 
-      var todo_item = buildTodo(response.content)
-      $('.todo_list').append(todo_item)
-      $('.todo').val('');
-    })
+        var todo_item = buildTodo(response.content)
+        $('.todo_list').append(todo_item)
+        $('.todo').val('');
+      })
 
-    request.fail(function(response) {
-      console.log('FAIL', response)
-    })
-
-  })
-
-  //var deleteToDoButtonListener
-  $('.delete').on('click', function(event) {
-    event.preventDefault();
-    // var todoName = $('.todo_list').children().children().first().text()
-    debugger
-
-    var request = $.ajax({
-      url: '/delete_todo',
-      type: 'delete',
-      dataType: 'json',
-      data: { todo: todoName }
-    })
-
-    request.done(function(response){
-      console.log('SUCCESS', response)
-
-      $(".todo_list").find("h2:contains("+response.todo_name+")").siblings().remove()
-      $(".todo_list").find("h2:contains("+response.todo_name+")").remove()
-    })
-
-    request.fail(function(response){
-      console.log('FAIL', response)
-    })
-  })
-
-  //var completeTodoButtonListener
-  $('.todo_list').on('click', '.complete', function(event) {
-    event.preventDefault();
-
-    var todoName = $('.todo_list').children().children().first().text()
-
-    var request = $.ajax({
-      url: '/complete_todo',
-      type: 'post',
-      dataType: 'json',
-      data: { todo: todoName }
-    })
-
-    request.done(function(response) {
-      console.log('SUCCESS', response)
-      $('.todo_list').find('h2:contains('+response.todo_name+')').siblings().children().last().text('COMPLETED!')
-    debugger
+      request.fail(function(response) {
+        console.log('FAIL', response)
+      })
 
     })
+  }
+  var deleteToDoButtonListener = function() {
+    $('#todo').on('click', '.delete', function(event) {
+      event.preventDefault();
 
-    request.fail(function(response) {
-      console.log('FAIL', response)
+      var route = $(this).attr('href');
+      var todo = $(this).closest('.todo');
+      var todoName = $(todo).children().first().text()
+
+      var request = $.ajax({
+        url: route,
+        type: 'delete',
+        dataType: 'json',
+        data: { todo: todoName }
+      })
+
+      request.done(function(response){
+        console.log('SUCCESS', response)
+
+      debugger
+
+        // $(".todo_list").find("h2:contains("+response.todo_name+")").siblings().remove()
+        // $(".todo_list").find("h2:contains("+response.todo_name+")").remove()
+        $(".todo_list").find("h2:contains("+response.todo_name+")").closest('.todo').remove()
+      })
+
+      request.fail(function(response){
+        console.log('FAIL', response)
+      })
     })
-  })
+  }
 
-}
+  var completeTodoButtonListener = function() {
+    $('.todo_list').on('click', '.complete', function(event) {
+      event.preventDefault();
+
+      var todoName = $('.todo_list').children().children().first().text()
+
+      var request = $.ajax({
+        url: '/complete_todo',
+        type: 'post',
+        dataType: 'json',
+        data: { todo: todoName }
+      })
+
+      request.done(function(response) {
+        console.log('SUCCESS', response)
+        $('.todo_list').find('h2:contains('+response.todo_name+')').siblings().children().last().text('COMPLETED!')
+      debugger
+
+      })
+
+      request.fail(function(response) {
+        console.log('FAIL', response)
+      })
+    })
+  }
+// }
 
 function buildTodo(todoName) {
   // gets todoTemplate stored in DOM.
