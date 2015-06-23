a53c909c01109c6f8e17abf041dc2014da99a46a
Nicholas Toulouse <faintsoundofwater@gmail.com>
2015-06-22 21:36:10 -0700

    wrote basic todo route and rendered list, now for ajax

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,9 +1,20 @@
 get '/' do
   # Look in app/views/index.erb
+  @todos = Todo.all
   erb :index
 end
 
 post '/add_todo' do
   p "Inside /add_todo route!"
+  @todo = Todo.new(todo_content: params[:todotext])
+
+  if @todo.save
+    # redirect '/'
+    # h = {id: @todo.id, todo_content: todo.todo_content}.to_json
+    # returns to the ajax call
+  else
+    status 500
+  end
+
 end
 
