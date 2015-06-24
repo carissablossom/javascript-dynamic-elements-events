c9026d5036e3b1084f292763717fd090325caa1a
cusackalex <cusackalex@gmail.com>
2015-06-23 17:45:16 -0700

    ajax complete works

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,9 +1,37 @@
 get '/' do
-  # Look in app/views/index.erb
+  @all_todos = Todo.all
   erb :index
 end
 
-post '/add_todo' do
-  p "Inside /add_todo route!"
+post '/todos' do
+  p params
+  todo = Todo.new(todo_content: params[:content])
+  if todo.save
+    # content_type :json
+    # todo.to_json
+    erb :'_todo_item', :locals => {:todo => todo }, layout: false
+  else
+    status 500
+  end
 end
+
+put '/todos/:todo_id' do
+  p "in put route"
+  p params
+  todo = Todo.where(id: params[:todo_id]).first
+  p todo
+  todo.completed = true
+  if todo.save
+    status 200
+  else
+    status 500
+  end
+end
+
+
+
+delete '/todos/:todo_id' do
+  Todo.find(params[:todo_id]).delete
+end
+
 
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -1,12 +1,84 @@
 $(document).ready(function() {
-  bindEvents();
+  $('.toolbox').on('submit', function(event){
+    event.preventDefault();
+    addTodoItem.call(this)
+  });
+
+  var todoList = $('.todo_list')
+
+  todoList.on('click', '.delete', function(event){
+    event.preventDefault()
+    var parent = $(this).closest('div')
+    var url = $(this).attr('href')
+    var request = $.ajax({
+      url: url,
+      method: 'delete',
+    });
+
+    request.done(function(response){
+      console.log(response)
+      parent.remove()
+    });
+
+    request.fail(function(response){
+      console.log(response)
+    })
+
+  });
+
+
+  var todoList = $('.todo_list')
+
+  todoList.on('click', '.complete', function(event){
+    event.preventDefault()
+    var url = $(this).attr('href')
+    var request = $.ajax({
+      url: url,
+      method: 'put',
+    });
+
+    request.done(function(response){
+      console.log(response)
+    });
+
+    request.fail(function(response){
+      console.log(response)
+    })
+
+  });
+
+
+
+
+
+  var addTodoItem = function(){
+    var form = $(this)
+    var formInput = $(this).find('input[name=content]')
+    var string = $(this).find('input[name=content]').val();
+    var request = $.ajax({
+      url: "/todos",
+      method: "post",
+      data: {content: string},
+    });
+
+    request.done(function(response){
+      $('.todo_list').append(response)
+      formInput.val('')
+    })
+
+    request.fail(function(response){
+      debugger
+    })
+
+  };
+
 });
 
 
-function bindEvents() {
-  // Bind functions which add, remove, and complete todos to the appropriate
-  // elements
-}
+// function bindEvents() {
+//   // Bind functions which add, remove, and complete todos to the appropriate
+//   // elements
+// }
 
 function buildTodo(todoName) {
   // gets todoTemplate stored in DOM.
