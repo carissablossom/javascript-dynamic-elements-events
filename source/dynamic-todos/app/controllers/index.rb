3100dcd3d9ec56a33d19dff3f7450577ac9d0439
karanaditya993 <karanaditya993@gmail.com>
2015-06-23 20:01:21 -0700

    Add delete ajax

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,9 +1,35 @@
+# INDEX
 get '/' do
-  # Look in app/views/index.erb
+  redirect '/todo/new'
+end
+
+# NEW
+get '/todo/new' do
+  @todos = Todo.all
   erb :index
 end
 
-post '/add_todo' do
-  p "Inside /add_todo route!"
+# CREATE
+post '/todo' do
+  @todo = Todo.new(todo_content: params[:content])
+  if @todo.save
+    content_type :json
+    (erb :_todo, :layout => false).to_json
+    # {content: @todo.todo_content}.to_json
+  else
+    status 400
+    "Please enter a valid todo"
+  end
 end
 
+# DELETE
+delete '/todo/:id' do
+  content_type :json
+  @todo = Todo.find(params[:id])
+  if @todo.destroy
+    {id: @todo.id}.to_json
+  else
+    status 400
+    "NO!!!"
+  end
+end
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -2,11 +2,64 @@ $(document).ready(function() {
   bindEvents();
 });
 
-
 function bindEvents() {
+  addTodo();
+  deleteTodo();
   // Bind functions which add, remove, and complete todos to the appropriate
   // elements
-}
+};
+
+var addTodo = function(){
+  $('form').on("submit", function(event){
+      event.preventDefault();
+      // var form = $(this);
+      // $('form') and it was giving :
+      //[form, context: form]
+      var form = $(this).serialize();
+      var path = $(this).attr('action');
+      var method = $(this).attr('method');
+      //$('form').serialize() and it was giving:
+      //content=whatever is inside the box
+      // Serialize turns complex data (i.e. form) into a query string
+      var request = $.ajax({
+        url: path,
+        type: method,
+        dataType: 'json',
+        data: form
+      })
+      request.done(function(response){
+        console.log(response, "Success!");
+        $('.todo_list').append(response);
+      });
+      request.fail(function(response){
+        console.log(response);
+      });
+    });
+};
+
+var deleteTodo = function(){
+  $('.todo_list').on("click", '.delete', function(event){
+    event.preventDefault();
+    var path = $(event.target)
+    var task = $(event.target).closest("div").attr("id")
+    // console.log(task);
+
+    var request = $.ajax({
+      url: $(path).attr("href"),
+      type: $(path).attr("class"),
+      data: {id: task}
+    });
+    request.done(function(response){
+      console.log(response);
+      console.log("success");
+      $('#'+response.id).remove();
+    });
+    request.fail(function(response){
+      console.log("fAIL");
+      console.log(response.statusText);
+    });
+  });
+};
 
 function buildTodo(todoName) {
   // gets todoTemplate stored in DOM.
